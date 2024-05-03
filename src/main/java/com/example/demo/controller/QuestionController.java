b07c7b200252c5979da153af20e3cbc06cd18b5d
Satyajit <beherasatyajit090@gmail.com>
2024-05-03 14:15:56 +0530

    adding update opration for all Question b the help of ID and DB uery

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +49,15 @@ public class QuestionController {
 		return questionService.deleteQuestion(id);
 		
 	}
-
+	
+	@PutMapping("/update/{id}")
+	public String updateQuestion(@PathVariable Integer id, @RequestBody Question question) {
+		
+		
+		
+		return questionService.updateQuestion(id, question);
+		
+		
+	}
+      
 }
--- src/main/java/com/example/demo/service/QuestionService.java
+++ src/main/java/com/example/demo/service/QuestionService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,6 +36,36 @@ public class QuestionService {
 		// TODO Auto-generated method stub
 		questionDao.deleteById(id);
 		return "Successfuly deleted";
+	}
+
+	public String updateQuestion(Integer id, Question question) {
+		// TODO Auto-generated method stub
+		
+		 Optional<Question> byId = questionDao.findById(id);
+		
+		 if (byId.isPresent()) {
+			 
+			 Question que = byId.get();
+			 
+			 que.setOption1(question.getOption1());
+			 que.setOption2(question.getOption2());
+			 que.setOption3(question.getOption3());
+			 que.setOption4(question.getOption4());
+			 que.setQuestionTitle(question.getQuestionTitle());
+			 que.setCategory(question.getCategory());
+			 que.setDifficultyLevel(question.getDifficultyLevel());
+			 que.setRightAnswer(question.getRightAnswer());
+			 
+			 questionDao.save(que);
+			 
+			 return "successful updated";
+			 
+			
+		}
+		 else {
+		 
+		return "id not found";
+		 }
 	}
 
 	
